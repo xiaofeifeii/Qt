a681b76cce7ae43fd5e3e31d5d9b145d433bf3b6
yangyifei <yangyifei@oceiot.com>
2022-11-05 12:26:15 +0800

    增加标志牌编号

--- project/Code_conversion/mainwindow.cpp
+++ project/Code_conversion/mainwindow.cpp
@@ -11,6 +11,10 @@ MainWindow::MainWindow(QWidget *parent) :
    connect(&port, &QSerialPort::readyRead , this ,MainWindow::log);
 
     ui->setupUi(this);
+    foreach(const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+    {
+        ui->comboBox->addItem(info.portName());
+    }
 }
 
 MainWindow::~MainWindow()
@@ -58,17 +62,6 @@ void MainWindow::on_openSerial_clicked()
 
 }
 
-void MainWindow::on_send_clicked()
-{
-    QString txt_code=ui->lineEdit->text();
-    //    AA BB 02 0D 14 07 D1 00 00
-
-    txt_code = QString::number(txt_code.toInt(), 16).toUpper();
-    QByteArray ba = QByteArray::fromHex("AABB020D14")+QByteArray::fromHex(txt_code.toLatin1())+QByteArray::fromHex("0000");
-    port.write(ba);
-    qDebug()<<"txt_code:"<<ba;
-
-}
 
 
 void MainWindow::log()
@@ -86,3 +79,60 @@ void MainWindow::on_clean_clicked()
 {
     ui->textBrowser_log->clear();
 }
+
+
+
+void MainWindow::on_send_clicked()
+{
+    QString nid_code=ui->lineEdit_NID->text();
+    QString aid_code=ui->lineEdit_AID->text();
+    QString did_code=ui->lineEdit_DID->text();
+    //AA BB 02 0D(NID) 14(AID) 07 D1(DID) 00 00
+    nid_code = QString::number(nid_code.toInt(), 16).toUpper();
+    aid_code = QString::number(aid_code.toInt(), 16).toUpper();
+    did_code = QString::number(did_code.toInt(), 16).toUpper();
+
+    QByteArray ba = QByteArray::fromHex("AABB02")+
+                    QByteArray::fromHex(nid_code.toLatin1())+ QByteArray::fromHex(aid_code.toLatin1()) +QByteArray::fromHex(did_code.toLatin1()) +
+                    QByteArray::fromHex("0000");
+    port.write(ba);
+    qDebug()<<"NID_code:"<<ba;
+}
+
+
+void MainWindow::on_send_sign_NID_clicked()
+{
+//    AA BB 18 00 01 0D 00 00
+    QString txt_code=ui->lineEdit_sign_NID->text();
+
+    qDebug()<<txt_code.toInt()+1;
+    if((txt_code.toInt()>0)&&(txt_code.toInt()<16))
+    {
+
+        txt_code = QString::number(txt_code.toInt(), 16).toUpper();
+//        QByteArray ba = QByteArray::fromHex("AABB180001")+QByteArray::fromHex(txt_code.toLatin1())+QByteArray::fromHex("D1C6");
+        QByteArray ba = QByteArray::fromHex("180001")+QByteArray::fromHex(txt_code.toLatin1());
+        quint8 temp[20];
+        memcpy(&temp, ba.data(), ba.length());
+
+        temp[20]=crc16CheckValue(temp,ba.length());
+        memcpy( ba.data(),&temp, ba.length());
+        qDebug()<<"crc:"<<ba;
+        port.write(ba);
+        qDebug()<<"sign_NID_code:"<<ba;
+    }else {
+        QMessageBox::warning(NULL , "数据不合法", "NID范围:0~15");
+    }
+
+
+}
+
+void MainWindow::on_read_sign_NID_clicked()
+{
+//    AABB1800010D0000
+    QByteArray ba = QByteArray::fromHex("AABB1900010D0000");
+    port.write(ba);
+    qDebug()<<"read_NID_code:"<<ba;
+}
+
+
--- project/Code_conversion/mainwindow.h
+++ project/Code_conversion/mainwindow.h
@@ -5,6 +5,8 @@
 #include <QSerialPort>
 #include <QSerialPortInfo>
 
+
+#define CRC_DEFAULT_VALUE  0xFFFF
 namespace Ui {
 class MainWindow;
 }
@@ -16,6 +18,27 @@ class MainWindow : public QMainWindow
 
 public:
     QSerialPort port;
+    quint16 crc16CheckValue(quint8 *pucBuf, quint16 usLen)
+    {
+        quint16 usCrc = CRC_DEFAULT_VALUE;
+        quint8 ucTmp = 0;
+        int i = 0,j = 0;
+        for (i = 0; i < usLen; i++) {
+            ucTmp = ((*pucBuf) & 0X00FF);
+            ++pucBuf;
+            usCrc ^= ucTmp;
+            for (j = 0; j < 8; j++) {
+                if (usCrc & 0X0001) {
+                    usCrc >>= 1;
+                    usCrc ^= 0XA001;
+                } else {
+                    usCrc >>= 1;
+                }
+            }
+        }
+        return usCrc;
+    }
+
 
 
 public:
@@ -29,11 +52,15 @@ private slots:
 
     void on_openSerial_clicked();
 
-    void on_send_clicked();
-
     void log();
 
     void on_clean_clicked();
+
+    void on_send_sign_NID_clicked();
+
+    void on_read_sign_NID_clicked();
+
+    void on_send_clicked();
 
 private:
     Ui::MainWindow *ui;
